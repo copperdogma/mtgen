a1030c61e82a3b9affcb52276b9c8962be5c1e61
Cam Marsollier <cam.marsollier@gmail.com>
2016-10-11 23:30:33 -0600

    - added new card-exception-generator that allows easy creation of import exceptions from a url and a pattern string

--- src/mtgen/wwwroot/util/js/card-data-importer.js
+++ src/mtgen/wwwroot/util/js/card-data-importer.js
@@ -1129,7 +1129,40 @@ var cardDataImporter = (function (my, $) {
         return finalImages;
     }
 
+    //CAMKILL: replaced by getCardImagesFromWotcArticle
     function getLandImagesFromWotcArticle(rawHtmlImageData, requiredImageWidth, requiredImageHeight) {
+        var image;
+        var finalImages = [];
+
+        var $images = $(rawHtmlImageData);
+
+        // v1 - 20150914, bfz gallery -- hard to scan as anything unique is added by js
+        if (finalImages.length < 1) {
+            var $rawimages = $images.find('#content img');
+            if ($rawimages.length > 0) {
+                var $imageContainer, $cardTitle;
+                var requiredImageHeightInt = parseInt(requiredImageHeight, 10);
+                var requiredImageWidthInt = parseInt(requiredImageWidth, 10);
+                _.each($rawimages, function (img) {
+                    if (!isNaN(requiredImageHeightInt) && requiredImageHeightInt !== img.height) return true;
+                    if (!isNaN(requiredImageWidthInt) && requiredImageWidthInt !== img.width) return true;
+                    image = {};
+                    image.src = img.src;
+                    image.height = img.height;
+                    image.width = img.width;
+                    finalImages.push(image);
+                });
+            }
+        }
+
+        _.each(finalImages, function (image) {
+            image.imageSource = "wotc-article";
+        });
+
+        return finalImages;
+    }
+
+    function getCardImagesFromWotcArticle(rawHtmlImageData, requiredImageWidth, requiredImageHeight) {
         var image;
         var finalImages = [];
 
@@ -1408,6 +1441,232 @@ var cardDataImporter = (function (my, $) {
         return cards;
     }
 
+    // Image-to-Exception importer -------------------------------------------------------------------------------------------
+
+    my.loadImagesAndGenerateExceptions = function (options) {
+        $.extend(my, options);
+
+        if (my.cardImageUrl === undefined || my.cardImageUrl.length < 1) {
+            alert("ERROR: No card image url supplied. Cannot continue.");
+            return;
+        }
+        if (my.cardPattern === undefined || my.cardPattern.length < 1) {
+            alert("ERROR: No card pattern supplied. Cannot continue.");
+            return;
+        }
+
+        // load all files and don't continue until all are loaded
+        var promises = [];
+
+        $.ajaxSetup({ timeout: 5000 });
+
+        promises.push($.get('/proxy?u=' + encodeURIComponent(my.cardImageUrl)));
+
+        my.trigger('data-loading');
+
+        $.when.apply($, promises).done(function (data) {
+            // the first result is essential
+            var cardImages = {
+                data: data,
+                urlSource: my.cardImageUrl
+            }
+            if (isBadResponse(cardImages.data)) {
+                alert("ERROR: No data retrieved from " + my.cardImageUrl + ". Response:" + cardImages);
+                return;
+            }
+
+            var setCode = my.setCode.trim();
+
+            my.trigger('data-loaded');
+
+            setTimeout(function () {
+                createExceptionOutputJson(setCode, cardImages, my.requiredImageWidth, my.requiredImageHeight, my.startingCardNum, my.cardPattern);
+            }, 100); // delay to let ui render
+        });
+
+    }
+
+    function getLandTypeFromCode(code) {
+        switch (code.toLowerCase()) {
+            case "w": return "Plains";
+            case "u": return "Island";
+            case "b": return "Swamp";
+            case "r": return "Mountain";
+            case "g": return "Forest";
+            default: return "Unknown type: " + code + " -- should be one of: w u r b g";
+        }
+    }
+
+    function createExceptionOutputJson(setCode, cardImageData, requiredImageWidth, requiredImageHeight, startingCardNum, cardPattern) {
+        // Get image data -------------------------------------------------------------------------------------------------
+        var cardImages = my.api.getCardImagesFromWotcArticle(cardImageData.data, requiredImageWidth, requiredImageHeight);
+        var imageDataCount = Object.size(cardImages);
+
+        // Parse the card pattern into an array.
+        var overrideItems = cardPattern.replace(/(?:\r\n|\r|\n)/g, ',');
+        var cardPatterns = overrideItems.trim().split(',');
+
+        // Create land cards out of each image
+        var cards = {};
+        var skippedCards = [];
+        var cardNum = startingCardNum;
+        var hasFixedCardNums = cardNum !== undefined && cardNum !== "";
+        var cardPatternIndex = 0;
+        var areLand = false;
+        var areTokens = false;
+        _.each(cardImages, function (image, index) {
+            var card = {};
+
+            Object.assign(card, image); // src, height, width, imageSource
+
+            // If overrides exist, use the entire pattern given, e.g.: w250,u251,b252,r253,g254 etc -- or x to skip a card
+            var skipCard = false;
+            if (cardPatterns.length <= cardPatternIndex) {
+                card.title = "Ran out of Card Pattern entries";
+            }
+            else {
+                // Determine the type of pattern.
+                var pattern = cardPatterns[cardPatternIndex].trim();
+
+                // x = skip a card
+                var skip = /x/gi.test(pattern);
+                if (skip) {
+                    skipCard = true;
+                    card.skippedCardIndex = index;
+                    skippedCards.push(card);
+                }
+                else {
+                    // e.g.: g107, or just g (and it will then use the default starting number)
+                    var land = /^([w|u|g|b|r])([0-9]{3})?$/gi.exec(pattern);
+                    if (land !== null && land.length > 0) {
+                        card.title = getLandTypeFromCode(land[1]);
+                        card.num = land[2];
+                        areLand = true;
+                    }
+                    else {
+                        // e.g.: 007|c|Token Artifact Creature|Thopter, or without the 007 and it'll use default starting number
+                        var cardPattern = /^([0-9]{3})?\|?(.)\|(.*)\|(.*)$/gi.exec(pattern);
+                        if (cardPattern === null || cardPattern.length === 0) {
+                            card.title = "Unknown pattern: " + pattern;
+                        }
+                        else {
+                            card.num = cardPattern[1];
+                            card.colour = cardPattern[2];
+                            card.type = cardPattern[3];
+                            card.title = cardPattern[4];
+                            card.subtype = cardPattern[4].replace(" Emblem", "");
+                            // ** make sure it all works for lands
+                        }
+                        areTokens = true;
+                    }
+                }
+            }
+            cardPatternIndex++;
+
+            if (!skipCard) {
+                card.matchTitle = mtgGen.createMatchTitle(card.title);
+
+                if (hasFixedCardNums) {
+                    card.num = cardNum++;
+                }
+
+                cards = addCardToCards(cards, card);
+            }
+        });
+
+        cards = _.sortBy(cards, 'num');
+
+        // Reporting -------------------------------------------------------------------------------------------------
+
+        var out = "";
+        var cardsLength = Object.size(cards);
+        if (cardsLength < 1) {
+            out += "<p>WARNING: No images found at url.</p>";
+        }
+
+        if (skippedCards.length > 0) {
+            out += "<p>The following " + skippedCards.length + " cards were skipped due to 'x's in your Card Patterns setting:</p><ul class='skipped-cards'>";
+            $.each(skippedCards, function (index, card) {
+                out += "<li><img src='" + card.src + "' height='" + Math.round(card.height / 2) + "' width='" + Math.round(card.width / 2) + "' />";
+                out += "<p>Card #" + (card.skippedCardIndex + 1) + "</p></li>";
+            });
+            out += "</ul>";
+        }
+
+        my.trigger('log-complete', out);
+
+        // Final JSON output -------------------------------------------------------------------------------------------------
+
+        var finalOut = [];
+        _.each(cards, function (card) {
+            delete card.matchTitle;
+            delete card.srcOriginal;
+            delete card.imageSourceOriginal;
+            delete card.fixedViaException;
+            delete card.imageSource;
+            delete card.mtgenId;
+            if (card.height === 370) { delete card.height; }
+            if (card.width === 265) { delete card.width; }
+
+            // Create the card as an exception.
+            var exception = {
+                "add": true,
+                "newValues": card
+            };
+            finalOut.push(exception);
+        });
+
+        // If we output anything, output the final card to apply the default values to all previous Basic Lands.
+        if (finalOut.length > 0) {
+            if (areLand) {
+                var postCard =
+                {
+                    "_comment": "Set basic land defaults for above lands so we don't have to repeat them every land",
+                    "where": "title=(Plains|Island|Swamp|Mountain|Forest)",
+                    "newValues": {
+                        "set": "{{setCode}}",
+                        "height": 370,
+                        "width": 265,
+                        "type": "Basic Land",
+                        "subtype": "{{title}}",
+                        "colour": "l",
+                        "cost": "",
+                        "rarity": "c",
+                        "num": "{{num}}/264 L"
+                    }
+                };
+                finalOut.push(postCard);
+            }
+            else if (areTokens) {
+                var postCard =
+                {
+                    "where": "",
+                    "newValues": {
+                        "set": "{{setCode}}",
+                        "rarity": "c",
+                        "num": "{{num}}/012 T",
+                        "token": true,
+                        "usableForDeckBuilding": false
+                    }
+                };
+                finalOut.push(postCard);
+            }
+        }
+
+        var jsonMainStr = JSON.stringify(finalOut, null, ' ');
+
+        var cardsHtmlSample = '';
+        _.each(cards, function (card) {
+            cardsHtmlSample += "<div class='card'><img src='" + card.src + "' height='"
+                + (card.height) + "' width='" + (card.width) + "' /><p>"
+                + card.num + ":" + card.title + "</p></div>";
+        });
+
+        my.trigger('data-processing-complete', cardsLength, jsonMainStr, cardsHtmlSample);
+    }
+
+
+    //CAMKILL: get rid of all of this; now replaced with loadImagesAndGenerateExceptions
     // Land files -------------------------------------------------------------------------------------------
 
     my.loadAndProcessLandFile = function (options) {
@@ -1706,6 +1965,7 @@ var cardDataImporter = (function (my, $) {
         getImagesFromMtgJsonData: getImagesFromMtgJsonData,
         getImagesFromCardsMainData: getImagesFromCardsMainData,
         getLandImagesFromWotcArticle: getLandImagesFromWotcArticle,
+        getCardImagesFromWotcArticle: getCardImagesFromWotcArticle,
 
         applyExceptions: applyExceptions,
         applyImagesToCards: applyImagesToCards
