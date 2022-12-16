81986390e32903320452007f769d13b8046b6c85
Cam Marsollier <cam.marsollier@gmail.com>
2022-12-16 16:16:43 -0700

    Card import now treats colourless cards that aren't artifacts but have a CMC as the "colourless" colour intead of "other""Jumpstart 2022
    - Done for set J22 where cards like Karn Liberated were coming up as Other despite wotc categorizing them as Colorless.

--- mtgen/wwwroot/util/js/card-data-importer.js
+++ mtgen/wwwroot/util/js/card-data-importer.js
@@ -4,6 +4,7 @@ Generates an output mtgen card set in json format for use in the main app, using
 Typically the importer file (e.g. import-main.json) will specify the wotc card gallery as the image source and
 the mtgsalvation spoiler page as the data source.
 
+20221216: Card import now treats colourless cards that aren't artifacts but have a CMC as the "colourless" colour intead of "other""
 20221109: Updated wotc The List importer to use new wotc html layout.
 20221107: Updated wotc image importer to use new wotc html layout.
 20220420: Will now import The List entries with no link.
@@ -780,6 +781,10 @@ class CardDataImporter {
                 else if (card.colour !== undefined && card.colour.length === 0) {
                     finalColour = isArtifact ? mtgGen.colours.artifact.code : mtgGen.colours.generic.code;
                 }
+                // Some cards (e.g.: J22 Karn Liberated) have no casting colour but aren't artifacts, so we'll force "colourness".
+                else if (uniqueColours.length < 1 && card.ccost > 0) {
+                    finalColour = 'c';
+                }
                 else {
                     finalColour = card.colour;
                 }
@@ -791,6 +796,7 @@ class CardDataImporter {
                 finalColour = mtgGen.colours.multicolour.code;
                 break;
         }
+
         return finalColour;
     }
 
@@ -799,7 +805,11 @@ class CardDataImporter {
         const hasCardFaces = card.cardFaces && card.cardFaces.length > 0;
         const colourIdentity = hasCardFaces ? card.cardFaces[0].color_identity.concat(card.cardFaces[1].color_identity) : card.color_identity
         const uniqueColours = [...new Set(colourIdentity)];
-        return uniqueColours.join('');
+
+        // Some cards (e.g.: J22 Karn Liberated) have no casting colour but aren't artifacts, so we'll force "colourness".
+        const finalColours = (uniqueColours.length < 1 && card.ccost > 0) ? [ "c" ] : [];
+
+        return finalColours.join('');
     }
 
     async _getCardData(cardData, cardDataUrlSource, setCode, options) {
